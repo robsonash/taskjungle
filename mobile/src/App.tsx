d550557fa7d24f3ea3d4ca717bc87e44815b26c5
Robson <robsoncs211@gmail.com>
2024-08-20 18:15:33 -0300

    Massacre mobile

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,32 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
+import {Button, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  Text,
-  View,
-} from 'react-native';
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {enableScreens} from 'react-native-screens';
+import CHeader from './components/CHeader/CHeader';
+// Chame enableScreens() antes de renderizar qualquer componente
+enableScreens();
 
-import CHeader from './components/CHeader/Cheader';
+// Defina o tipo de navegação para a tela Home
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
+// Defina a tela HomeScreen
+const HomeScreen: React.FC<Props> = ({navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
+
+const DetailsScreen: React.FC<Props> = ({navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details </Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+};
+// Crie o stack navigator
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <CHeader title="Header" />
-        </View>
-        <View>
-          <Text>Oi</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
