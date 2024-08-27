af51ae0360b06061bee36b09155fe8a5a02d896d
Robson <robsoncs211@gmail.com>
2024-08-27 14:34:23 -0300

    Front task

--- front/src/app/lib/axiosConfig.ts
+++ front/src/app/lib/axiosConfig.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
 
-// Obter o token do localStorage (se existir)
-const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+// Função para obter o token dos cookies (caso exista)
+function getCookie(name: string): string | undefined {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop()?.split(';').shift();
+  }
+  return undefined;
+}
+
+// Obter o token dos cookies (se existir)
+const token = typeof window !== "undefined" ? getCookie("token") : null;
 
 const api = axios.create({
   baseURL: "http://localhost:3001",
@@ -14,7 +24,8 @@ if (token) {
 
 // Interceptador para adicionar o token nas requisições
 api.interceptors.request.use((config) => {
-  const newToken = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  console.log('config', config)
+  const newToken = typeof window !== "undefined" ? getCookie("token") : null;
   if (newToken) {
     config.headers["Authorization"] = `Bearer ${newToken}`;
   }
--- front/src/app/tasks/page.tsx
+++ front/src/app/tasks/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 
 import LinkButton from "@/components/LinkButton";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Task {
+  _id: string;
+  name: string;
+}
 
 export default function TasksPage() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  async function getTasks() {
+    const response = await axios.get<Task[]>("http://localhost:3001/tasks");
+    setTasks(response.data);
+  }
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   return (
     <main className="container mx-auto">
       <h1 className="my-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900">
-        tasks
+        Tasks
       </h1>
       <LinkButton label="Voltar para a tela principal" href="/" />
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>{task.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
